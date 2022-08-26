080260abe31e991ece3a62ec32c55c87e6c41d59
hanjiawang <1301671498@qq.com>
2022-08-26 21:39:04 +0800

    feat: isRef && unRef && proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -7,6 +7,7 @@ class Refimpl {
   private _value: any;
   private _rawValue: any;
   public dep;
+  public __v_isRef = true;
   constructor(value) {
     this._rawValue = value;
     this._value = covert(value);
@@ -42,4 +43,27 @@ function trackRefValue(ref) {
 
 export function ref(value) {
   return new Refimpl(value);
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(refObj) {
+  return new Proxy(refObj, {
+    get: (target, key) => {
+      return unRef(Reflect.get(target, key));
+    },
+    set: (target, key, value) => {
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
 }
--- src/reactivity/test/ref.spec.ts
+++ src/reactivity/test/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from '../effect';
-import { ref } from '../ref';
+import { ref, isRef, unRef, proxyRefs } from '../ref';
+import { reactive } from '../reactive';
 
 describe('ref', () => {
   it('happy path', () => {
@@ -25,17 +26,52 @@ describe('ref', () => {
     expect(calls).toBe(2);
     expect(dummy).toBe(2);
   });
-});
 
-it('should make nested properties reactive', () => {
-  const a = ref({
-    count: 1,
+  it('should make nested properties reactive', () => {
+    const a = ref({
+      count: 1,
+    });
+    let dummy;
+    effect(() => {
+      dummy = a.value.count;
+    });
+    expect(dummy).toBe(1);
+    a.value.count = 2;
+    expect(dummy).toBe(2);
   });
-  let dummy;
-  effect(() => {
-    dummy = a.value.count;
+
+  it('isRef', () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
   });
-  expect(dummy).toBe(1);
-  a.value.count = 2;
-  expect(dummy).toBe(2);
+
+  it('unRef', () => {
+    const a = ref(1);
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
+
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(10),
+      name: 'xiaohong',
+    };
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.name).toBe('xiaohong');
+    expect(proxyUser.age).toBe(10);
+
+    proxyUser.age = 20;
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
+  });
 });
